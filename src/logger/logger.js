d49c5d2adaf9c40d8818fdd88e62e98751ee21d2
comrade31 <atulsinghgga98@gmail.com>
2022-10-15 19:08:50 +0530

    This is welcome function

---
+++ src/logger/logger.js
@@ -0,0 +1,11 @@
+// const  underscore = require("underscore")
+
+
+ const welcome = function welcome(){
+    return  "welcome to my application i am Atul and a part  of function up in reote.js lithium cohort." ;
+ };
+
+
+ module.exports.welcome = welcome
+
+
