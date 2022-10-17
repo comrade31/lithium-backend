e4511986c5c1a55d32b77ebde468d0813d1a00b0
comrade31 <atulsinghgga98@gmail.com>
2022-10-17 23:23:47 +0530

    Get api Assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,42 +2,153 @@ const express = require('express');
 const abc = require('../introduction/intro')
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    console.log('My batch is', abc.name)
-    abc.printName()
-    res.send('My second ever api!')
-});
 
-router.get('/students', function (req, res){
-    console.log("The path params in the request are : ", req.params)
-    let students = ['Sabiha', 'Neha', 'Akash']
-    res.send(students)
+// ------------- Problem 1 Movies APi ------------ 
+
+let myArray = ["Kantara","Ramayana","HankyPanky","Nayakan","JaiBhim"]
+
+router.get('/Movies', function (req, res) {
+    res.send(myArray)
 })
 
+// --------- Problem 2 Index number ---------
 
-// Example 1 for path params
-router.get('/students/:studentName', function(req, res){
-    // ':' denotes that the following part of route is a variable
-    // The value of this variable is what we are sending in the request url after /students
-    // This value is set in the form of an object inside req.params
-    // The object contain key value pairs
-    // key is the variable in the route
-    // value is whatever dynamic value sent in the request url
-    let myParams = req.params
+router.get('/Movies/:indexNumber',(req,res)=>{
+    let index = req.params.indexNumber  
+    res.send(myArray[index])
+ })
 
-    // params attribute is fixed in a request object
-    // params contains the path parameters object
-    console.log("The path params in the request are : ", myParams)
-    res.send('The full name is ' + myParams.studentName )
+// -------- Problem 3 error --------
+
+
+
+router.get('/Movies/:indexNumber', (req,res) => {
+    let index = req.params.indexNumber
+    if(index>myArray.length){
+        res.send("Please use a valid index");
+    }
+    else{
+        res.send(myArray[index])  
+    }
 })
 
-// Example 2 for path params
-router.get('/student-details/:name', function(req, res){
-    let requestParams = req.params
-    console.log("This is the request ", requestParams)
-    let studentName = requestParams.name
-    console.log('Name of the student is ', studentName)
-    res.send('Dummy response')
+// ---------Problem4 stead of an array of strings define an array of movie objects this time
+
+let films = [ {
+    'id': 1,
+    'name': 'The Shining'
+   }, {
+    'id': 2,
+    'name': 'Incendies'
+   }, {
+    'id': 3,
+    'name': 'Rang de Basanti'
+   }, {
+    'id': 4,
+    'name': 'Finding Nemo'
+   }]
+
+   router.get('/films', (req, res) => {
+    res.send(films)
+})
+
+// Problem 5
+
+router.get('/films/:filmid',(req,res)=>{
+    let filmid = req.params.filmid
+    if (filmid>films.length){
+        res.send('No movie exists with this id')
+    }
+    else{
+        res.send(films[filmid-1])
+    }
 })
 
 module.exports = router;
+
+
+
+///// Another Codes of same assignment ==>>
+
+// router.get('/movies', function (req,res){
+    
+//     const mo=["Midnightmass","Friday 13","NArnia","Harry Potter"]
+//     res.send(mo)
+// })
+
+// router.get('/movies/:index', function (req,res){
+//      let inParams = req.params
+//      console.log(inParams)
+//      const mo=["Midnightmass","Friday 13","NArnia","Harry Potter"]
+//      if(inParams.index>mo.length){
+//         res.send("right valid")
+//      }
+    
+    
+//     res.send(mo[inParams.index])
+// })
+
+// router.get('/films', function (req,res){
+    
+//     const fi=[ {
+//         id: 1,
+//         name: "The Shining"
+//        },
+//         {
+//         id: 2,
+//         name: "Incendies"
+//        }, {
+//         id: 3,
+//         name: "Rang de Basanti"
+//        }, {
+//         id: 4,
+//         name: "Finding Nemo"
+//        }]
+       
+//     res.send(fi)
+// })
+
+// router.get('/films/:id', function (req,res){
+
+//     let idParam=req.params
+    
+//     const fi=[ {
+//         id: 1,
+//         name: "The Shining"
+//        },
+//         {
+//         id: 2,
+//         name: "Incendies"
+//        }, {
+//         id: 3,
+//         name: "Rang de Basanti"
+//        }, {
+//         id: 4,
+//         name: "Finding Nemo"
+//        }]
+
+       
+//        if(idParam.id>(fi.length+1)||idParam.id==0){
+//         res.send("not valid")
+        
+//     }      
+       
+       
+//        for(i of fi){
+
+       
+        
+        
+//         if(i.id==idParam.id){
+//             console.log(i)
+//             res.send(i.name)
+//         }
+//        }
+//         // res.send("enter valid")
+       
+
+
+//     //    console.log(idParam)
+       
+//     // res.send(idParam.name)
+// })
