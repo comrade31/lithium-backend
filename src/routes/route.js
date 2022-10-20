8738bbd87b026efe2bf597e7e3ca5abbca01012c
comrade31 <atulsinghgga98@gmail.com>
2022-10-20 12:52:36 +0530

    all there assigment with player

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,73 +1,92 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/students/:name', function(req, res) {
-    let studentName = req.params.name
-    console.log(studentName)
-    res.send(studentName)
-})
+// Problem 1 Pritesh sir-
 
-router.get("/random" , function(req, res) {
-    res.send("hi there")
-})
+// let myArray1 = [1,2,3,5,6,7]
+// let n = 7
+// let mySum1 = n*(n+1)/2; // 28
+// let mysum2 = 0;
+// for(i=0; i<myArray1.length; i++) {
+//     mysum2 = mysum2+myArray1[i]; // 24
+// }
+// router.get('/Find', function(req, res){
+//     res.send(console.log(mySum1-mysum2));
+// })
 
+// Problem 2 Pritesh sir-
 
-router.get("/test-api" , function(req, res) {
-    res.send("hi FunctionUp")
-})
-
-
-router.get("/test-api-2" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API")
-})
-
-
-router.get("/test-api-3" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's ")
-})
-
-
-router.get("/test-api-4" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
+// let myArray3 = [33,34,35,37,38] // 36 is missing
+// let n = myArray3.length+1; // 6
+// let ToatlSum = (myArray3[0]+myArray3[myArray3.length-1])*n/2 // 213
+// let ToatlSum2 = 0;
+// for(i=0; i<myArray3.length; i++) {
+//     ToatlSum2 = ToatlSum2+myArray3[i]; // 177
+// }
+// router.get('/Find2', function(req, res){
+//     res.send(console.log(ToatlSum-ToatlSum2));
+// })
 
 
 
-router.get("/test-api-5" , function(req, res) {
-    res.send("hi FunctionUp5. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
-
-router.get("/test-api-6" , function(req, res) {
-    res.send({a:56, b: 45})
-})
-
-router.post("/test-post", function(req, res) {
-    res.send([ 23, 45 , 6])
-})
+// Problem 3 - Player Assignment =>
 
 
-router.post("/test-post-2", function(req, res) {
-    res.send(  { msg: "hi" , status: true }  )
-})
+let players =
+   [
+       {
+           "name": "manish",
+           "dob": "1/1/1995",
+           "gender": "male",
+           "city": "jalandhar",
+           "sports": [
+               "swimming"
+           ]
+       },
+       {
+           "name": "gopal",
+           "dob": "1/09/1995",
+           "gender": "male",
+           "city": "delhi",
+           "sports": [
+               "soccer"
+           ],
+       },
+       {
+           "name": "lokesh",
+           "dob": "1/1/1990",
+           "gender": "male",
+           "city": "mumbai",
+           "sports": [
+               "soccer"
+           ],
+       },
+   ]
 
-router.post("/test-post-3", function(req, res) {
-    // let id = req.body.user
-    // let pwd= req.body.password
+   router.post("/players1",function (req, res) {
+    let players2 = req.body.element
+     for (i of players) {
+         if (i.name === players2.name) {
+             return res.send({msg:"name already exists"})
+             
+         }
+     }
+ 
+     players.push(players2)
+     console.log(players2)
+     res.send({ msg: players, status: true })
+ 
+ })
 
-    // console.log( id , pwd)
 
-    console.log( req.body )
+ 
+//    router.post('/newPlayers', function(req,res) {
+//     let newp= req.body
+//     players.push(newp)
+//     res.send(players)
+//    })
 
-    res.send(  { msg: "hi" , status: true }  )
-})
 
-
-
-router.post("/test-post-4", function(req, res) {
-    let arr= [ 12, "functionup"]
-    let ele= req.body.element
-    arr.push(ele)
-    res.send(  { msg: arr , status: true }  )
-})
 
 module.exports = router;
+
