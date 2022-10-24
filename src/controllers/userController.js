fbbac3f5f4b125b2b317f3cd6d4aafcf6b9615de
comrade31 <atulsinghgga98@gmail.com>
2022-10-24 16:24:06 +0530

    I worte Schema inside models folder

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,15 +1,19 @@
+const { models } = require("mongoose")
 const UserModel= require("../models/userModel")
 
-const createUser= async function (req, res) {
-    let data= req.body
-    let savedData= await UserModel.create(data)
-    res.send({msg: savedData})
+
+const createBook= async function (req, res) {
+    let data = req.body
+   let saveData= await UserModel.create(data)
+    res.send({msz: saveData})
 }
 
-const getUsersData= async function (req, res) {
-    let allUsers= await UserModel.find()
-    res.send({msg: allUsers})
+
+const getBook=  async function (req, res) {
+    let allbooks = await UserModel.find()
+    res.send({msz: allbooks})
 }
 
-module.exports.createUser= createUser
-module.exports.getUsersData= getUsersData
+
+module.exports.createBook = createBook
+module.exports.getBook = getBook
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,31 +1,24 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema( {
-    firstName: String,
-    lastName: String,
-    mobile: {
+//// Assignment BookSchema -->
+
+const bookSchema = new mongoose.Schema({
+    bookName: {
         type: String,
-        unique: true,
+        required: true,
+        unique: true
+    },
+    authoreName: {
+        type: String,
         required: true
     },
-    emailId: String,
-    gender: {
-        type: String,
-        enum: ["male", "female", "LGBTQ"] //"falana" will give an error
+    category: {
+        String,
+        enum: ["science", "english", "math", "hindi", "others"]  
     },
-    age: Number,
-    // isIndian: Boolean,
-    // parentsInfo: {
-    //     motherName: String,
-    //     fatherName: String,
-    //     siblingName: String
-    // },
-    // cars: [ String  ]
+    year: Number
+
 }, { timestamps: true });
 
-module.exports = mongoose.model('User', userSchema) //users
+module.exports = mongoose.model('Book', bookSchema)  // Books
 
-
-
-// String, Number
-// Boolean, Object/json, array
